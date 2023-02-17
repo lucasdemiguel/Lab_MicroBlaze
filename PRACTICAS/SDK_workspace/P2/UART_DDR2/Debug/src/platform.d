7503af37aa9a3b4cf22a9880713951a81f6dca95
lucasdemiguel <47428229+lucasdemiguel@users.noreply.github.com>
2023-02-17 10:21:07 +0100

    Add files via upload

---
+++ PRACTICAS/SDK_workspace/P2/UART_DDR2/Debug/src/platform.d
@@ -0,0 +1,25 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../UART_DDR2_bsp/microblaze_0/include/xparameters.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/xil_cache.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/mb_interface.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/xil_types.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/xil_assert.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/xil_exception.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/xparameters.h \
+ ../src/platform_config.h
+
+../../UART_DDR2_bsp/microblaze_0/include/xparameters.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/xil_cache.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/mb_interface.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/xil_types.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/xil_assert.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/xil_exception.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/xparameters.h:
+
+../src/platform_config.h:
---
+++ PRACTICAS/SDK_workspace/P2/UART_DDR2/Debug/src/uart_ddr2.d
@@ -0,0 +1,63 @@
+src/uart_ddr2.o src/uart_ddr2.o: ../src/uart_ddr2.c \
+ ../../UART_DDR2_bsp/microblaze_0/include/xparameters.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/xil_printf.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/xil_types.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/xparameters.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/bspconfig.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/xil_exception.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/xuartlite.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/xil_assert.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/xstatus.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/xintc.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/xintc_l.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/xil_io.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/xil_printf.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/mb_interface.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/xil_exception.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/xgpio_l.h ../src/platform.h \
+ ../src/platform_config.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/xil_testmem.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/xil_io.h \
+ ../../UART_DDR2_bsp/microblaze_0/include/xil_assert.h
+
+../../UART_DDR2_bsp/microblaze_0/include/xparameters.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/xil_printf.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/xil_types.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/xparameters.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/bspconfig.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/xil_exception.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/xuartlite.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/xil_assert.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/xstatus.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/xintc.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/xintc_l.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/xil_io.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/xil_printf.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/mb_interface.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/xil_exception.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/xgpio_l.h:
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/xil_testmem.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/xil_io.h:
+
+../../UART_DDR2_bsp/microblaze_0/include/xil_assert.h:
