57086aeec8816eec439098af66f24edf64951769
lucasdemiguel <47428229+lucasdemiguel@users.noreply.github.com>
2023-02-17 10:22:12 +0100

    Add files via upload

---
+++ PRACTICAS/SDK_workspace/P3/RTL_MB/Debug/src/RTL_MB.d
@@ -0,0 +1,55 @@
+src/RTL_MB.o src/RTL_MB.o: ../src/RTL_MB.c \
+ ../../RTL_MB_bsp/microblaze_0/include/xparameters.h \
+ ../../RTL_MB_bsp/microblaze_0/include/xil_printf.h \
+ ../../RTL_MB_bsp/microblaze_0/include/xil_types.h \
+ ../../RTL_MB_bsp/microblaze_0/include/xparameters.h \
+ ../../RTL_MB_bsp/microblaze_0/include/bspconfig.h \
+ ../../RTL_MB_bsp/microblaze_0/include/xil_exception.h \
+ ../../RTL_MB_bsp/microblaze_0/include/xuartlite.h \
+ ../../RTL_MB_bsp/microblaze_0/include/xil_assert.h \
+ ../../RTL_MB_bsp/microblaze_0/include/xstatus.h \
+ ../../RTL_MB_bsp/microblaze_0/include/xintc.h \
+ ../../RTL_MB_bsp/microblaze_0/include/xintc_l.h \
+ ../../RTL_MB_bsp/microblaze_0/include/xil_io.h \
+ ../../RTL_MB_bsp/microblaze_0/include/xil_printf.h \
+ ../../RTL_MB_bsp/microblaze_0/include/mb_interface.h \
+ ../../RTL_MB_bsp/microblaze_0/include/xil_exception.h \
+ ../../RTL_MB_bsp/microblaze_0/include/xgpio_l.h \
+ ../../RTL_MB_bsp/microblaze_0/include/xgpio.h \
+ ../../RTL_MB_bsp/microblaze_0/include/xgpio_l.h
+
+../../RTL_MB_bsp/microblaze_0/include/xparameters.h:
+
+../../RTL_MB_bsp/microblaze_0/include/xil_printf.h:
+
+../../RTL_MB_bsp/microblaze_0/include/xil_types.h:
+
+../../RTL_MB_bsp/microblaze_0/include/xparameters.h:
+
+../../RTL_MB_bsp/microblaze_0/include/bspconfig.h:
+
+../../RTL_MB_bsp/microblaze_0/include/xil_exception.h:
+
+../../RTL_MB_bsp/microblaze_0/include/xuartlite.h:
+
+../../RTL_MB_bsp/microblaze_0/include/xil_assert.h:
+
+../../RTL_MB_bsp/microblaze_0/include/xstatus.h:
+
+../../RTL_MB_bsp/microblaze_0/include/xintc.h:
+
+../../RTL_MB_bsp/microblaze_0/include/xintc_l.h:
+
+../../RTL_MB_bsp/microblaze_0/include/xil_io.h:
+
+../../RTL_MB_bsp/microblaze_0/include/xil_printf.h:
+
+../../RTL_MB_bsp/microblaze_0/include/mb_interface.h:
+
+../../RTL_MB_bsp/microblaze_0/include/xil_exception.h:
+
+../../RTL_MB_bsp/microblaze_0/include/xgpio_l.h:
+
+../../RTL_MB_bsp/microblaze_0/include/xgpio.h:
+
+../../RTL_MB_bsp/microblaze_0/include/xgpio_l.h:
