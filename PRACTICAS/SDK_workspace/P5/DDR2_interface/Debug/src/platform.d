10f98650441caabb24d0701605612fd53f67c589
lucasdemiguel <47428229+lucasdemiguel@users.noreply.github.com>
2023-02-17 10:24:11 +0100

    Add files via upload

---
+++ PRACTICAS/SDK_workspace/P5/DDR2_interface/Debug/src/platform.d
@@ -0,0 +1,25 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../DDR2_interface_bsp/microblaze_0/include/xparameters.h \
+ ../../DDR2_interface_bsp/microblaze_0/include/xil_cache.h \
+ ../../DDR2_interface_bsp/microblaze_0/include/mb_interface.h \
+ ../../DDR2_interface_bsp/microblaze_0/include/xil_types.h \
+ ../../DDR2_interface_bsp/microblaze_0/include/xil_assert.h \
+ ../../DDR2_interface_bsp/microblaze_0/include/xil_exception.h \
+ ../../DDR2_interface_bsp/microblaze_0/include/xparameters.h \
+ ../src/platform_config.h
+
+../../DDR2_interface_bsp/microblaze_0/include/xparameters.h:
+
+../../DDR2_interface_bsp/microblaze_0/include/xil_cache.h:
+
+../../DDR2_interface_bsp/microblaze_0/include/mb_interface.h:
+
+../../DDR2_interface_bsp/microblaze_0/include/xil_types.h:
+
+../../DDR2_interface_bsp/microblaze_0/include/xil_assert.h:
+
+../../DDR2_interface_bsp/microblaze_0/include/xil_exception.h:
+
+../../DDR2_interface_bsp/microblaze_0/include/xparameters.h:
+
+../src/platform_config.h:
